5e85b9f60e37c20bc9bd881df94725e9ffe2dc62
ranjt12103 <ranjitppatil10@gmail.com>
2026-01-15 20:33:18 +0530

    models

--- Backend/store/admin.py
+++ Backend/store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Orders, OrederItem, Product, UserProfile
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(UserProfile)
+admin.site.register(Orders)
+admin.site.register(OrederItem)
